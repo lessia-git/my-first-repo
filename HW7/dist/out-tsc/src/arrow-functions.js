0cfca3ce94520b41d16e854b5acf56a335ef02b4
lessia-git <lessia.sobchenko@gmail.com>
2026-06-04 21:13:38 +0300

    deleted dist

--- HW7/dist/out-tsc/src/arrow-functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const addElements2 = (elements) => elements.reduce((total, element) => total + element, (typeof elements[0] === 'string' ? '' : 0));
-const strings2 = ['червоний', 'зелений', 'синій'];
-const numbers2 = [1, 2, 3, 4];
-console.log(addElements2(strings2));
-console.log(addElements2(numbers2));
-console.log(addElements2([5, 10]));
-//# sourceMappingURL=arrow-functions.js.map
